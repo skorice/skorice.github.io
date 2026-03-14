ca12c547a9afbc61bdf1476f09b88644a96d9923
skorice <99252434+skorice@users.noreply.github.com>
2026-03-14 10:07:57 +0300

    task 2 with tests

---
+++ WindowsFormApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(255, 241);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(452, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(98, 171);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(807, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите выражение для вычисления:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 367);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 558);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Вычисление выражений";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormApp2/WindowsFormsApp2/Parser.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace WindowsFormsApp2
+{
+    public class Parser
+    {
+        private string text;
+        private int pos;
+
+        /// <summary>
+        /// Вычисляет арифметическое выражение
+        /// </summary>
+        public double Start(string expr)
+        {
+            if (string.IsNullOrEmpty(expr)) throw new Exception("Пустое выражение");
+
+            text = expr;
+            pos = 0;
+            double result = ParsePlusMinus();
+
+            if (pos < text.Length)
+                throw new Exception($"Лишний символ '{text[pos]}' в конце");
+
+            return result;
+        }
+
+        // Сложение и вычитание
+        private double ParsePlusMinus()
+        {
+            double result = ParseMnojDel();
+
+            while (pos < text.Length)
+            {
+                char op = text[pos];
+                if (op != '+' && op != '-') break;
+
+                pos++;
+                if (pos < text.Length && IsOperator(text[pos]))
+                    throw new Exception($"Два оператора подряд");
+
+                double term = ParseMnojDel();
+                result = op == '+' ? result + term : result - term;
+            }
+            return result;
+        }
+
+        // Умножение и деление
+        private double ParseMnojDel()
+        {
+            double result = ParseSkobki();
+
+            while (pos < text.Length)
+            {
+                char op = text[pos];
+                if (op != '*' && op != '/') break;
+
+                pos++;
+                if (pos < text.Length && IsOperator(text[pos]))
+                    throw new Exception($"Два оператора подряд");
+
+                double factor = ParseSkobki();
+
+                if (op == '*')
+                    result *= factor;
+                else
+                {
+                    if (factor == 0) throw new DivideByZeroException("Деление на ноль");
+                    result /= factor;
+                }
+            }
+            return result;
+        }
+
+        // Скобки, унарные знаки, числа
+        private double ParseSkobki()
+        {
+            if (pos >= text.Length) throw new Exception("Неожиданный конец");
+
+            char ch = text[pos];
+
+            // Унарный плюс/минус
+            if (ch == '+' || ch == '-')
+            {
+                if (pos > 0 && text[pos - 1] == ')')
+                    throw new Exception($"Некорректный унарный оператор");
+
+                pos++;
+                double val = ParseSkobki();
+                return ch == '+' ? val : -val;
+            }
+
+            // Скобки
+            if (ch == '(')
+            {
+                pos++;
+                double res = ParsePlusMinus();
+
+                if (pos >= text.Length || text[pos] != ')')
+                    throw new Exception("Нет закрывающей скобки");
+
+                pos++;
+                return res;
+            }
+
+            // Лишняя закрывающая скобка
+            if (ch == ')') throw new Exception("Лишняя закрывающая скобка");
+
+            // Число
+            return ParseChislo();
+        }
+
+        // Чтение числа
+        private double ParseChislo()
+        {
+            int start = pos;
+            bool dot = false;
+
+            while (pos < text.Length)
+            {
+                char ch = text[pos];
+                if (char.IsDigit(ch))
+                    pos++;
+                else if ((ch == '.' || ch == ',') && !dot)
+                {
+                    dot = true;
+                    pos++;
+                }
+                else break;
+            }
+
+            if (start == pos) throw new Exception("Ожидалось число");
+
+            string num = text.Substring(start, pos - start).Replace(',', '.');
+            double.TryParse(num, System.Globalization.NumberStyles.Any,
+                           System.Globalization.CultureInfo.InvariantCulture, out double res);
+            return res;
+        }
+
+        // Проверка на оператор
+        private bool IsOperator(char c) => c == '+' || c == '-' || c == '*' || c == '/';
+    }
+}
