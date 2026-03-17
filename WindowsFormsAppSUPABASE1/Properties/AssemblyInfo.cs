bcc6a96aaf73cf7177515984ea5b12cfb0b40d4d
skorice <99252434+skorice@users.noreply.github.com>
2026-03-17 10:45:56 +0300

    Супабаза регистрация и вход

---
+++ WindowsFormsAppSUPABASE1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("WindowsFormsAppSUPABASE1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsAppSUPABASE1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e46521a6-a178-4a82-b279-b66f195c5e56")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WindowsFormsAppSUPABASE1/SupabaseService.cs
@@ -0,0 +1,26 @@
+﻿using Supabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppSUPABASE1
+{
+    internal class SupabaseService
+    {
+        public static async Task InitializeAsync()
+        {
+
+            var url = "https://hfirhbncadzydvmyatjy.supabase.co";
+            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmaXJoYm5jYWR6eWR2bXlhdGp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNzg0MjUsImV4cCI6MjA4ODk1NDQyNX0.Np1NgHDKYQR8L2aRIPvfOkztfaRKctcD8R00g9hyD3w";
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+            var supabase = new Supabase.Client(url, key, options);
+            var res= await supabase.InitializeAsync();
+        }
+    }
+}
+
