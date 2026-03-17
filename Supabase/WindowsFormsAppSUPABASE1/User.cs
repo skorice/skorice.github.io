6dcddcc8613c74c18b14e03f75b89674afd9f6c8
skorice <99252434+skorice@users.noreply.github.com>
2026-03-17 10:49:44 +0300

    Supabase (регистрация и вход)

---
+++ Supabase/WindowsFormsAppSUPABASE1/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace WindowsFormsAppSUPABASE1
+{
+    internal class User
+    {
+        [Table("users")]
+        public class UserModel : BaseModel
+        {
+            [PrimaryKey("username")]
+            [Column("username")]
+            public string Name { get; set; }
+
+            [Column("password")]
+            public string Password { get; set; }
+        }
+    }
+}
